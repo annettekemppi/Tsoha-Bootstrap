f7f9d21da8b9ee7a4380487de25e50cf4b507a35
Kemppi Annette M <annettek@da5-cs-bk107-25.helsinki.fi>
2016-03-21 16:18:06 +0200

    Sovellus on alustettu ja versionhallinta otettu käyttöön!

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="annettek"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="tsohameininki"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
