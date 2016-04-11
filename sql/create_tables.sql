714fe471f3d4bc491b4577527651ee59f37b9950
Kemppi Annette M <annettek@da5-cs-bk107-25.helsinki.fi>
2016-04-11 19:10:26 +0300

    tietokantataulujen luontiyritys

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,19 @@
-CREATE TABLE Kayttaja () {
-
+CREATE TABLE Kayttaja ( 
     id SERIAL PRIMARY KEY;
-    name VARCHAR(50) NOT NULL;
-    password VARCHAR(50) NOT NULL;
+    name varchar(50) NOT NULL;
+    password varchar(50) NOT NULL;
+
+);
+
+CREATE TABLE Rotu (
+  id SERIAL PRIMARY KEY,
+  rotu_id INTEGER REFERENCES Race(id), -- Viiteavain Player-tauluun
+  name varchar(50) NOT NULL,
+  status boolean DEFAULT FALSE,
+  description varchar(400),
+  published DATE,
+  publisher varchar(50),
+  added DATE
+);
 
 }-- Lisää CREATE TABLE lauseet tähän tiedostoon
