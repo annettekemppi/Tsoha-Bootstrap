339dc009da08f028043dfc6b47b3f6d464f809a9
Kemppi Annette M <annettek@da5-cs-bk107-25.helsinki.fi>
2016-04-11 18:56:11 +0300

    ain laulain työtäs tee!

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -9,5 +9,17 @@ class HelloWorldController extends BaseController {
     public static function sandbox() {
         View::make('helloworld.html');
     }
+    
+    public static function race_list(){
+    View::make('suunnitelmat/race_list.html');
+  }
+
+  public static function race_show(){
+    View::make('suunnitelmat/race_show.html');
+  }
+
+  public static function login(){
+    View::make('suunnitelmat/login.html');
+  }
 
 }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/race', function() {
+  HelloWorldController::race_list();
+  });
+  
+  $routes->get('/race/1', function() {
+  HelloWorldController::race_show();
+  });
+
+  $routes->get('/login', function() {
+  HelloWorldController::login();
+  });
