bcbbd9955ad5f3833b291753c83876a5161685fd
Kemppi Annette M <annettek@da3-cs-dk110-02.helsinki.fi>
2016-05-13 13:30:07 +0300

    koulussa tehdyt

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,7 +1,7 @@
 -- Kayttaja-taulun testidata
-INSERT INTO Kayttaja (id, name, password, admin) VALUES ('1', 'Mealy', 'papumuhennos', false); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Kayttaja (name, password, admin) VALUES ('Mealy', 'papumuhennos', true); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
 -- Rotu-taulun testidata
-INSERT INTO Rotu (id, name, status, description, published, country, added) VALUES ('1', 'Alaskanmalamuutti', 'Rekisteröity', 'Raskaiden kuormien vetokoira', '2011-11-11', 'USA', NOW());
+INSERT INTO Rotu (name, registered, description, published, country, added) VALUES ('Alaskanmalamuutti', true, 'Raskaiden kuormien vetokoira', '2011-11-11', 'USA', NOW());
 -- Roturyhma-taulun testidata
-INSERT INTO Roturyhma (id, rotu_id, name, count, class) VALUES ('1', '3', 'Terrierit', '5', 'FCI 3');
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Roturyhma (kayttaja_id, rotu_id, name, count, class) VALUES (1, 1, 'Terrierit', '5', 'FCI 3');
+-- Lisää INSERT INTO lauseet tähän tiedosto
